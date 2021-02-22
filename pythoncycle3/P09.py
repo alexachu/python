59c56c6d1836cd074867bd15bca61748ff66c9ac
alexachu <74653508+alexachu@users.noreply.github.com>
2021-02-22 10:09:26 +0530

    Add files via upload

---
+++ pythoncycle3/P09.py
@@ -0,0 +1,20 @@
+#Construct following pattern using nested loop.
+#*
+#**
+#***
+#****
+#*****
+#****
+#***
+#**
+#*
+rows=5
+for i in range(0,rows):
+    for j in range(0,i+1):
+        print("*", end=' ')
+    print("\r")
+
+for i in range(rows,0,-1):
+    for j in range(0,i-1):
+        print("*", end=' ')
+    print("\r")
